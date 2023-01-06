c328e88bc766ba66e98fe15d5a2786e35504fa39
redstormfx <b.rumyanzev@gmail.com>
2023-01-06 14:09:40 +0200

    error heandling added

--- app.js
+++ app.js
@@ -10,35 +10,46 @@ app.use(express.json());
 const PORT = 3001;
 
 app.get("/metadata", async (req, res) => {
-  const { googleSheets, auth, spreadsheetId } =
-    await Authorization.getAuthSheets();
-  const metadata = await googleSheets.spreadsheets.get({
-    auth,
-    spreadsheetId,
-  });
-  res.send(metadata.data);
+  try {
+    const { googleSheets, auth, spreadsheetId } =
+      await Authorization.getAuthSheets();
+    const metadata = await googleSheets.spreadsheets.get({
+      auth,
+      spreadsheetId,
+    });
+    res.status(200).send(metadata.data);
+  } catch (e) {
+    res.status(300).send("spreadsheetId or credentials.jsone is invalid");
+  }
 });
 
 app.post("/addRow", async (req, res) => {
   const { googleSheets, auth, spreadsheetId } =
     await Authorization.getAuthSheets();
-
-  const row = await googleSheets.spreadsheets.values.append({
-    auth,
-    spreadsheetId,
-    range: "'Лист1'!A1:C1",
-    valueInputOption: "USER_ENTERED",
-    resource: {
-      values: [],
-    },
-  });
-  res.send(row.data);
+  try {
+    const row = await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: "'Test'!A1:C1",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: getDataResult,
+      },
+    });
+    res.send(row.data);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 });
 
 app.post("/start", async (req, res) => {
-  let browserInstance = browserObject.startBrowser();
-  scraperController(browserInstance);
-  res.status(200).send("Parsing is running");
+  try {
+    let browserInstance = browserObject.startBrowser();
+    scraperController(browserInstance);
+    res.status(200).send("Parsing is running");
+  } catch (e) {
+    res.status(500).send(e.message, "Parsing process crashed");
+  }
 });
 app.get("/result", async (req, res) => {
   res.send(getDataResult);
